81f4dcbe2b3e4427ea4620ff97cd7656feff779a
bhaktiagrawal088 <bhaktiagrawal286@gmail.com>
2023-10-19 14:51:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// console.log("hello world");
+// var myname = "Bhakti Agrawal";
+// var myage = 25
+// var b = true
+// console.log(myname);
+// console.log(myage);
+// console.log(typeof(myname))
+// console.log(typeof(myage))
+// console.log(typeof(b))
+// var year = 2020;
+// if((year%4 == 0 && year%100!=0) || year%400==0){
+//     console.log(`${year} is a leap Year`);
+// }
+// else{
+//     console.log(` ${year} is not a Leap Year`) ;
+// }
+// let num = prompt("enter the number");
+// var num = 1;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+// var num = 8;
+// for(i = 1; i<=10; i++){
+//     console.log(num + " x " + i + " = " + num*i );
+// }
+
+
+// function sum( a,  b){
+//     return (a+b);}
+//     console.log(sum(3 ,6));
+
+// var n = ("Enter a number :");
+// console.log(n);
+
+// var num = 8;
+// for(var  i=1;i<=10;i++){
+//     console.log(`${num} X ${i} = ${num * i}`);
+
+// }
+
+// const sum = () => `sum is the ${(a=9)+(b=8)}`
+// console.log(sum());
+
+
+let arr = [1,2,3,4,5,6,'ram'];
+for(var i in arr){
+    console.log(arr[i]);
+}
